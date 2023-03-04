d136a745455423d2848e81a0107c08e8a09f3e8c
agustin emilio <agustinesimiand@gmail.com>
2023-03-04 20:36:35 -0300

    ejercicio1 bot

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        int result;
+        result = suma(10, 20, 30);
+        System.out.println(result);
+
+
+    }
+
+    public static int suma(int a, int b, int c) {
+        return a + b + c;
+    }
+
+}
